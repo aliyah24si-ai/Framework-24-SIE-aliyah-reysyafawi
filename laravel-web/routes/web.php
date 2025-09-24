af0444182c805b31f8e2fe864cdc19f2b203fc7a
Aliyahsie <ALiyah24SIi@Mahasiswa.pcr.ac.id>
2025-09-24 08:08:01 +0700

    Starter Projek Laravel

---
+++ laravel-web/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+Route::get('/nama/{param1}/{nim?}', function ($param1,$nim= '') {
+    return 'Nama saya: '.$param1. '<br>Nim :' .$nim;
+
+});
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1  ;
+});
+Route::get('/about', function () {
+    return view('halaman-about');
+});
