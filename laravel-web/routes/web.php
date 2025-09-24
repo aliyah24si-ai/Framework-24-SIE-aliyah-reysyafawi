b78bd31a01d1600c7522679b030986dfe60714fb
Aliyahsie <ALiyah24SIi@Mahasiswa.pcr.ac.id>
2025-09-24 09:51:28 +0700

    Penambahan Home Blade

--- laravel-web/routes/web.php
+++ laravel-web/routes/web.php
@@ -20,4 +20,6 @@ Route::get('/nim/{param1?}', function ($param1 = '') {
 Route::get('/about', function () {
     return view('halaman-about');
 });
+use App\Http\Controllers\HomeController;
+
 Route::get('/home',[HomeController::class,'index'] );
